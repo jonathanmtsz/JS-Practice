e45d8fba34c18fe592ae758646ffca34c2c2a016
jonathanmtsz <jonathanmtsz@outlook.com>
2023-03-16 19:49:22 -0300

    testes json

--- jsontestes/encontrar.js
+++ jsontestes/encontrar.js
@@ -1,6 +1,6 @@
 const clientes = require("./clientes.json");
 function encontrar (lista, chave, valor){
-    return lista.find((item) => item[chave] === valor);
+    return lista.find((item) => item[chave].includes(valor));
 }
 
 const encontrado = encontrar(clientes, "nome", "Kirby");
@@ -8,4 +8,4 @@ const encontrado = encontrar(clientes, "nome", "Kirby");
 const encontrado2 = encontrar(clientes, "telefone", "23789892634");
 
 
-console.log(encontrado2)
+console.log(encontrado)
